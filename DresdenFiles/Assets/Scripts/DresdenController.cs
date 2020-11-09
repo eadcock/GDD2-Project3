1953301266d689231d4a103543f94fa86ce1bde2
Eric Adcock <eta4411@rit.edu>
2020-11-09 18:32:40 -0500

    Dresden movement & health system

---
+++ DresdenFiles/Assets/Scripts/DresdenController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using quiet;
+
+public class DresdenController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime;
+        transform.position = transform.position.RemoveZDim() + movement * speed;
+    }
+}
