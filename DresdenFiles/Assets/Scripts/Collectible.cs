7dfc4cc83c6b65c5e6bbb0d1cfa243e11b24ba6c
Peyton Anderson <pia1208@rit.edu>
2020-11-15 16:16:09 -0500

    Implemented basic collectible (health and clue) and inventory system

---
+++ DresdenFiles/Assets/Scripts/Collectible.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using quiet;
+
+public class Collectible : MonoBehaviour
+{
+    public GameObject player;
+    public string type;
+
+    private Health pHealth;
+    private Inventory inv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pHealth = player.GetComponent<Health>();
+        inv = player.GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if colliding with the player, pick up
+        if (quiet.Collision.BoundingCircle(gameObject.transform.position, 0.1f, player.transform.position, 0.1f))
+        {
+            if (type == "clue")
+            {
+                // add to inventory
+                inv.AddItem(this);
+                this.gameObject.SetActive(false);
+            }
+            else if (type == "health")
+            {
+                // heal player
+                // 30?
+                pHealth.Heal(30);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ DresdenFiles/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Collectible> inv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inv = new List<Collectible>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(Collectible item)
+    {
+        inv.Add(item);
+    }
+}
