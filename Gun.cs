2d63b89a70f455685844387e696474e16c9b2b47
Alex Gray <47251731+Dennis-TheMenace@users.noreply.github.com>
2020-11-17 11:37:34 -0500

    Now?

---
+++ Gun.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    Bullet b = new Bullet();
+    int bullets = 6;
+    float reload = 6;
+    public Rigidbody bulletBody;
+    private DresdenController d;
+    Vector3 screenPos = new Vector3();
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        d = GetComponent<DresdenController>();
+        screenPos = Camera.main.WorldToScreenPoint(bulletBody.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && bullets != 0)
+        {
+            b = new Bullet();
+            Rigidbody clone;
+            clone = Instantiate(bulletBody, new Vector3(d.transform.position.x, d.transform.position.y, 0), Quaternion.identity);
+            clone.velocity = transform.TransformDirection(Vector3.right * 10);
+            bullets--;
+        }
+        
+        if(bullets == 0)
+        {
+            reload -= Time.deltaTime;
+        }
+        if(reload <=0)
+        {
+            bullets = 6;
+            reload = 6;
+        }
+
+    }
+}//delegate d
+
+public class Bullet
+{
+    int damage;
+    bool active;
+    float x;
+    float y;
+
+    public Bullet()
+    {
+        damage = 15;
+        active = true;
+        x = 0;
+        y = 0;
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+    }
+
+    public bool Active
+    {
+        get { return active; }
+        set { active = value; }
+    }
+
+    public float X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public float Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+}
---
+++ Spells.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spells : MonoBehaviour
+{
+    bool fire = true;
+    bool lightning = false;
+    bool buttonHold = false;
+    bool lightningCreate = false;
+    float fireCoolDown = 0;
+    float lightningCoolDown = 0;
+    public Rigidbody fireBall;
+    public Transform lightningBolt;
+    private DresdenController d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        d = GetComponent<DresdenController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Select spell
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            fire = true;
+            lightning = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            fire = false;
+            lightning = true;
+        }
+
+        //Use spell
+        if (!InputManager.isPaused && Input.GetMouseButtonDown(0))
+        {
+            if (fire == true && fireCoolDown <= 0)
+            {
+                FireBall ball = new FireBall();
+                Rigidbody clone;
+                clone = Instantiate(fireBall, new Vector3(d.transform.position.x, d.transform.position.y, -1), Quaternion.identity);
+                clone.velocity = transform.TransformDirection(Vector3.right * 10);
+                fireCoolDown = 1;
+            }
+            if (lightning == true && lightningCoolDown <= 0)
+            {
+                Lightning bolt = new Lightning();
+                if (lightningCreate == false)
+                {
+                    lightningBolt = Instantiate(lightningBolt, new Vector3(d.transform.position.x, d.transform.position.y, 0), Quaternion.identity);
+                    lightningCreate = true;
+                }
+                buttonHold = true;
+                lightningCoolDown = 10;
+            }
+        }
+
+        //Cooldowns
+        fireCoolDown -= Time.deltaTime;
+        lightningCoolDown -= Time.deltaTime;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            buttonHold = false;
+        }
+
+        if(buttonHold == true)
+        {
+            lightningBolt.transform.position = d.transform.position;
+        }
+        else
+        {
+            lightningBolt.transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
